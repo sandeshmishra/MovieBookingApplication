5763f41c3ca01107ac83153b1ff633b24d35f4c8
sandesh <sandesh.11717772@gmail.com>
2021-09-14 20:49:04 +0530

    checkpoint 3

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Header.css';
-
 class Header extends React.Component{
     render(){
         return(
             <div className="headerCSS">
-            <img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"></img>
+            
+            <img id='logo' src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"></img>
             </div>
         )
     }
 }
-
-
-export default Header; 
+export default Header;
